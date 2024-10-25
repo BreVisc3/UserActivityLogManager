db707e9f1bdea9a3b2ff9500c8d374eb4d9f637c
Brendan Viscount <bpviscou@ncsu.edu>
2024-10-25 12:46:54 -0400

    [Testing] Got the constructor coverage that was missing

--- UserActivityLogManager/test/edu/ncsu/csc316/activity/manager/TestUserActivityLogManager.java
+++ UserActivityLogManager/test/edu/ncsu/csc316/activity/manager/TestUserActivityLogManager.java
@@ -1,5 +1,48 @@
 package edu.ncsu.csc316.activity.manager;
 
-public class TestUserActivityLogManager {
+import org.junit.Before;
+import org.junit.Test;
 
+import edu.ncsu.csc316.activity.dsa.DataStructure;
+
+public class TestUserActivityLogManager {
+	
+	private UserActivityLogManager def;
+	private UserActivityLogManager type;
+	
+	@Before
+	public void setUp() {
+		def = new UserActivityLogManager("LOGIN1.TXT");
+		type = new UserActivityLogManager("LOGIN2.TXT", DataStructure.UNORDEREDLINKEDMAP);
+	}
+	
+	@Test
+	public void TestGetEntriesByDate() {
+		
+	}
+
+	@Test
+	public void TestGetEntriesByHour() {
+		
+	}
+	
+	@Test
+	public void TestGetTopActivities() {
+		
+	}
+	
+	@Test
+	public void TestGetActionList() {
+		
+	}
+	
+	@Test
+	public void TestGetReturnArray() {
+		
+	}
+	
+	@Test
+	public void TestGetLargest() {
+		
+	}
 }
